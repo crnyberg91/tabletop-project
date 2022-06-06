95a1e5a8b6f649f34239367e81fc8c7ee4b38998
Chris Nyberg <chrisn83@gmail.com>
2022-06-06 15:48:37 -0400

    bootstrap installed, container components imported

---
+++ src/features/MainContentBox/MainTabs/mainTabs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './mainTabs.css';
+
+
+
+class MainTabs extends React.Component {
+    render() {
+        // console.log(this.props);
+        return (
+            <div>Main Tabs Coming Soon</div>
+        );
+    }
+}
+
+export default MainTabs;
